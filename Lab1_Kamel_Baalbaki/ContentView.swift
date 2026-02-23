b16103e241c22ffb8b9a214bbcc23c6c8d8fe893
KamelBaalbaki <baalbaki.kamel@gmail.com>
2026-02-23 01:36:46 -0500

    Starting Prime Number App Project

---
+++ Lab1_Kamel_Baalbaki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Kamel_Baalbaki
+//
+//  Created by Kamel Baalbaki on 2026-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Kamel_Baalbaki/Lab1_Kamel_BaalbakiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Kamel_BaalbakiApp.swift
+//  Lab1_Kamel_Baalbaki
+//
+//  Created by Kamel Baalbaki on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Kamel_BaalbakiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
