9ebfcaae448a9181f89b7e62edbe14b66a069703
KamelBaalbaki <baalbaki.kamel@gmail.com>
2026-02-23 02:19:25 -0500

    Adding an Alert Diaglog when the game is over

--- Lab1_Kamel_Baalbaki/ContentView.swift
+++ Lab1_Kamel_Baalbaki/ContentView.swift
@@ -8,64 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @StateObject var game = GameLogic()
-    
+
     var body: some View {
-        
+
         VStack(spacing: 40) {
-            
+
             Text("Prime Number Game")
                 .font(.largeTitle)
                 .bold()
-            
+
             Text("\(game.currentNumber)")
                 .font(.system(size: 80))
                 .fontWeight(.bold)
-            
+
             if game.showResultIcon {
-                
+
                 Image(systemName: game.isCorrectSelection ? "checkmark.circle.fill" : "xmark.circle.fill")
                     .resizable()
                     .frame(width: 90, height: 90)
                     .foregroundColor(game.isCorrectSelection ? .green : .red)
             }
-        }
-        HStack(spacing: 30) {
-            Button {
-                game.selectAnswer(isPrimeSelected: true)
-            } label: {
-                
-                Text("Prime")
-                    .frame(width: 140, height: 50)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            
-            Button {
-                game.selectAnswer(isPrimeSelected: false)
-            } label: {
-                
-                Text("Not Prime")
-                    .frame(width: 140, height: 50)
-                    .background(Color.orange)
-                    .foregroundColor(.white)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-        }
-        
-        VStack(spacing: 10) {
 
-                        Text("Correct: \(game.correctAnswers)")
-                        Text("Wrong: \(game.wrongAnswers)")
-                        Text("Attempts: \(game.attempts)/10")
+            HStack(spacing: 30) {
 
-                    }
-                    .font(.headline)
+                Button {
+                    game.selectAnswer(isPrimeSelected: true)
+                } label: {
 
+                    Text("Prime")
+                        .frame(width: 140, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                 }
+
+                Button {
+                    game.selectAnswer(isPrimeSelected: false)
+                } label: {
+
+                    Text("Not Prime")
+                        .frame(width: 140, height: 50)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+
+            VStack(spacing: 10) {
+
+                Text("Correct: \(game.correctAnswers)")
+                Text("Wrong: \(game.wrongAnswers)")
+                Text("Attempts: \(game.attempts)/10")
+
+            }
+            .font(.headline)
+
+        }
+        .padding()
+        .alert("Game Finished", isPresented: $game.showDialog) {
+
+            Button("Play Again") {
+                game.resetGame()
+            }
+
+        } message: {
+
+            Text("Correct Answers: \(game.correctAnswers)\nWrong Answers: \(game.wrongAnswers)")
+        }
+    }
 }
 
 #Preview {
